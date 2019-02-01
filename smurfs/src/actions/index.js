7af3e901e546f4114fd6ca1aa9239b5a9b625c96
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-01 11:11:00 -0600

    Created actions for adding a new smurf as well as an action creator for adding a new smurf

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -6,6 +6,9 @@ import axios from 'axios';
 export const FETCHING_SMURFS = 'FETCHING_SMURFS';
 export const FETCHING_SMURFS_SUCCESS = 'FETCHING_SMURFS_SUCCESS';
 export const FETCHING_SMURFS_FAILURE = 'FETCHING_SMURFS_FAILURE';
+export const ADDING_SMURF = 'ADDING_SMURF';
+export const ADDING_SMURF_SUCCESS = 'ADDING_SMURF_SUCCESS';
+export const ADDING_SMURF_FAILURE = 'ADDING_SMURF_FAILURE';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -23,4 +26,12 @@ export const getSmurfs = () => dispatch => {
     .get('http://localhost:3333/smurfs')
     .then(res => dispatch({ type: FETCHING_SMURFS_SUCCESS, payload: res.data }))
     .catch(err => dispatch({ type: FETCHING_SMURFS_FAILURE, payload: err }))
+};
+
+export const addNewSmurf = smurf => dispatch => {
+  dispatch({ type: ADDING_SMURF });
+  axios
+    .post('http://localhost:3333/smurfs', smurf)
+    .then(res => dispatch({ type: ADDING_SMURF_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: ADDING_SMURF_FAILURE, payload: err }))
 };
