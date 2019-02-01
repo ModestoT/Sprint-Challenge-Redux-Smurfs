5c2f57059e7c5112b96fb0e29609dc4285414086
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-01 11:37:13 -0600

    Set up the ability to add a smurf to the data base

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { getSmurfs } from '../actions';
+import { getSmurfs, addNewSmurf } from '../actions';
 import './App.css';
 /*
  to wire this component up you're going to need a few things.
@@ -11,19 +11,52 @@ import './App.css';
  */
 class App extends Component {
 
+  state = {
+      name: '',
+      age: '',
+      height: '' 
+  };
+
+  handleInput = e => {
+    let target = e.target.placeholder.toLowerCase();
+
+    switch(target){
+      case 'name':
+        return this.setState({ name: e.target.value });
+      case 'age':
+        return this.setState({ age: e.target.value });
+      case 'height':
+        return this.setState({ height: e.target.value });
+      default: 
+        return "invalid input";
+    }
+  }
+  addSmurf = e => {
+    e.preventDefault();
+    const smurf = { name: this.state.name, age: this.state.age, height: this.state.height }
+    this.props.addNewSmurf(smurf);
+  }
+
   componentDidMount() {
     this.props.getSmurfs();
   }
+
   render() {
     return (
       <div className="App">
+        <form onSubmit={e => this.addSmurf(e)}>
+          <input type="text" placeholder="Name" onChange={this.handleInput}/>
+          <input type="number" placeholder="Age" onChange={this.handleInput}/>
+          <input type="number" placeholder="Height" onChange={this.handleInput}/>
+          <button>Add Smurf</button>
+        </form>
         <ul className="smurfs-list">
           {this.props.smurfs.map(smurf => {
             return (
                 <div className="smurf" key={smurf.id}> 
                   <h2>{smurf.name}</h2>
-                  <li>{smurf.age}</li>
-                  <li>{smurf.height}</li>
+                  <li>Age: {smurf.age}</li>
+                  <li>Height: {smurf.height}</li>
                 </div>
             );
           })}
@@ -38,4 +71,4 @@ const mapStateToProps = state => ({
 })
 export default connect(
   mapStateToProps, 
-  { getSmurfs })(App);
+  { getSmurfs, addNewSmurf })(App);
